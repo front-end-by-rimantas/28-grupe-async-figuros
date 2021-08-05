d2f89148e9f7fc5b6953c45b62e3756411dc9417
Rimantas <belauzas@yahoo.com>
2021-08-05 20:18:12 +0300

    Solution

--- js/Figura.js
+++ js/Figura.js
@@ -1,10 +1,22 @@
 class Figura {
     static randomFiguros(amount) {
+        const list = [];
 
+        for (let i = 0; i < amount; i++) {
+            const width = Figura.random();
+            const height = Figura.random();
+            list.push({ width, height });
+        }
+
+        return list;
+    }
+
+    static random() {
+        return Math.floor(Math.random() * 10) + 1;
     }
 
     static size(width, height) {
-
+        return width * height;
     }
 }
 
